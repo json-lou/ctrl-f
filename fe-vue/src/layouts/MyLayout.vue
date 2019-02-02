52d614c60e7bc26c6ff369695d75a5f137404dcc
Brittany Lau <brittanyplau@gmail.com>
2019-02-02 12:39:45 -0500

    Initialize Vue app

---
+++ fe-vue/src/layouts/MyLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="primary"
+        :glossy="$q.theme === 'mat'"
+        :inverted="$q.theme === 'ios'"
+      >
+        <q-toolbar-title>
+          QHacks 2019
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
